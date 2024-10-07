ebf276912e8d8714626d44dbe8e32285e88c7b55
mokheyre <125134292+mokheyre@users.noreply.github.com>
2024-10-07 14:20:38 +0100

    Update requests.js
    Re added activities

--- services/requests.js
+++ services/requests.js
@@ -43,7 +43,12 @@ const addActivityToDB = (req, res) => {
     .then(result => res.json(result))
     .catch(err => console.log(err));
 }
-
+const deleteAllActivites = (req, res) => {
+  const removeString = 'DELETE FROM "my_activities"'; // delete all items in the 'my_activities' table
+  pool.query(removeString) // send query delete all items in the 'my_activities' table
+    .then(res.send('All activities cleared!')) // send confirmation to the browser
+    .catch(err => console.log(err));  
+}
 
 
 module.exports = { getSingleActivity, addActivityToDB, getAllActivities, deleteAllActivites }
